d9302419f29caa46a57abf61c0288b99a639ed42
Guillaume Marc <marcguillaume77@gmail.com>
2025-02-20 17:08:53 +0100

    Learn about vue's event handling

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
+      showBooks: true,
       title: "As I Lay Dying",
       author: "Faulkner",
       age: "64",
     };
+  },
+  methods: {
+    changeTitle() {
+      console.log("clicked");
+      this.title = "Some title boiiii";
+    },
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
   },
 });
 
