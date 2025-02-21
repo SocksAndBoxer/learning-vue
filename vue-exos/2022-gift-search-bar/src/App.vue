edef8ca5b97ab4a801b81e53ae7d7130018b202c
Guillaume Marc <marcguillaume77@gmail.com>
2025-02-21 11:12:05 +0100

    Display list using with search debounce

--- vue-exos/2022-gift-search-bar/src/App.vue
+++ vue-exos/2022-gift-search-bar/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
+import { useDebounceFn } from '@vueuse/core'
 
 const productApi = 'https://dummyjson.com/products/search'
 
@@ -7,35 +8,38 @@ type Product = {
   brand: string
   price: number
   title: string
+  id: number
 }
 
-async function fetchApi(searchTerm: string) {
+const products = ref<Product[]>([])
+const isLoading = ref(false)
+const searchTerm = ref('')
+
+async function fetchProducts(searchTerm: string) {
   const res = await window.fetch(`${productApi}/?q=${searchTerm}`)
   const json = await res.json()
-  console.log(json)
   return json.products as Product[]
 }
 
-async function getProducts() {
-  const products = await fetchApi('phone')
-  console.log(products)
+async function findProducts(searchTerm: string) {
+  products.value = await fetchProducts(searchTerm)
+  isLoading.value = false
 }
 
-getProducts()
+watch(searchTerm, () => (isLoading.value = true))
 
-const searchTerm = ref('')
-
-const findProducts = async term => {}
-
-watch(searchTerm, newTerm => findProducts(newTerm))
+watch(searchTerm, useDebounceFn(findProducts, 500))
 </script>
 
 <template>
   <div class="w-full h-full flex flex-col gap-5 justify-center items-center">
     <h1 class="text-4xl font-bold">Gift Search Bar</h1>
     <input type="text" class="p-2 border-2 border-gray-dark" v-model="searchTerm" placeholder="Start typing..." />
-    <ul class="list-disc">
-      <li>Display suggestions here</li>
+    <ul v-if="!isLoading" class="list-disc">
+      <li v-for="product in products" :key="product.id">
+        {{ product.title }} - {{ product.brand }}: {{ product.price }}
+      </li>
     </ul>
+    <p v-else>Loading...</p>
   </div>
 </template>
