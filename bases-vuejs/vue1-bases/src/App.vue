bccf4217720537370e5990739c6018b834f50e46
Guillaume Marc <marcguillaume77@gmail.com>
2025-02-25 17:34:26 +0100

    Lean if conditions

--- bases-vuejs/vue1-bases/src/App.vue
+++ bases-vuejs/vue1-bases/src/App.vue
@@ -2,31 +2,19 @@
 import { ref } from "vue"
 
 const message = ref("Tuto Vue.js")
-const inputMessage = ref("Texte exemple")
-
-function submitInputMessage() {
-  console.log(inputMessage.value);
-}
+const sun = ref(true)
 
 </script>
 
 <template>
   <main>
     <h1>{{ message }}</h1>
-    <label class="input-message" for="toto">
-      Saisir un texte
-      <input v-model="inputMessage" type="text" name="toto">
-    </label>
-    <button @click="submitInputMessage">Valier</button>
-    <p>{{ inputMessage }}</p>
+    <h2 v-if="sun">Jour</h2>
+    <h2 v-else>Nuit</h2>
+    <button @click="sun = !sun">Toggle</button>
   </main>
 </template>
 
 <style scoped>
-.input-message {
-  display: flex;
-  flex-direction: column;
-  max-width: 400px;
-}
 
 </style>
