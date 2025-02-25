58040c219d5972ffc736adab696573a63bb0e398
Guillaume Marc <marcguillaume77@gmail.com>
2025-02-25 18:16:18 +0100

    Learn list interactions

--- bases-vuejs/vue1-bases/src/App.vue
+++ bases-vuejs/vue1-bases/src/App.vue
@@ -17,14 +17,32 @@ const items = ref([
     id: id++
   }
 ])
+const newItem = ref("")
+
+function addItem() {
+ if (newItem.value.length === 0) return
+
+//  items.value = [...items.value, { text: newItem.value, id: id++ } ]
+ items.value.push({ text: newItem.value, id: id++ })
+ newItem.value = '' 
+}
+
+function removeItem(itemToDelete) {  
+  items.value = items.value.filter((item) => item !== itemToDelete)
+}
 </script>
 
 <template>
   <main>
     <h1>{{ message }}</h1>
     <ul>
-      <li v-for="item in items" :key="item.id">{{ item.text }}</li>
+      <li v-for="item in items" :key="item.id">
+        {{ item.text }}
+        <button @click="removeItem(item)">X</button>
+      </li>
     </ul>
+    <input type="text" v-model="newItem">
+    <button v-show="newItem.length > 0" @click="addItem">Ajouter à la liste</button>
   </main>
 </template>
 
