bb5e37b7313b048e1a4a836db82eccf4cac35ffb
Guillaume Marc <marcguillaume77@gmail.com>
2025-02-25 10:51:06 +0100

    Learn about dynamic attributes

--- bases-vuejs/vue1-bases/src/App.vue
+++ bases-vuejs/vue1-bases/src/App.vue
@@ -7,14 +7,27 @@ const count = ref(0)
 function incrementCount() {
   count.value += 1
 }
+
+function resetCount() {
+  count.value = 0
+}
 </script>
 
 <template>
   <main>
     <h1>{{ message }}</h1>
-    <h2>Nombre = {{ count }}</h2>
+    <h2 :id="'id_'+count" :class="count < 10 ? 'blueviolet' : 'red'">Nombre = {{ count }}</h2>
     <button @click="incrementCount">Incrémenter la valeur</button>
+    <button :style="count <= 0 && 'display:none;'" @click="resetCount">Réinitialiser la valeur</button>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .blueviolet {
+    color: blueviolet
+  }
+
+  .red {
+    color: red;
+  }
+</style>
