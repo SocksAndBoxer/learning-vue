fb8697cf222628e42b03efa94a9afff09ea9b655
Guillaume Marc <marcguillaume77@gmail.com>
2025-02-25 10:50:39 +0100

    Learn ref and click event

--- bases-vuejs/vue1-bases/src/App.vue
+++ bases-vuejs/vue1-bases/src/App.vue
@@ -1,9 +1,20 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue"
+
+const message = ref("Modifier le nombre")
+const count = ref(0)
+
+function incrementCount() {
+  count.value += 1
+}
+</script>
 
 <template>
-  <header></header>
-
-  <main></main>
+  <main>
+    <h1>{{ message }}</h1>
+    <h2>Nombre = {{ count }}</h2>
+    <button @click="incrementCount">Incrémenter la valeur</button>
+  </main>
 </template>
 
 <style scoped></style>
