8035a464a9b02f0b699d71382ec037df062c28b4
Guillaume Marc <marcguillaume77@gmail.com>
2025-02-26 20:43:01 +0100

    Learn about components

--- bases-vuejs/vue1-bases/src/App.vue
+++ bases-vuejs/vue1-bases/src/App.vue
@@ -1,51 +1,53 @@
 <script setup>
-import { ref } from "vue"
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
-let id = 0
-const message = ref("Tuto Vue.js")
+let id = 0;
+const message = ref("Tuto Vue.js");
 const items = ref([
-  { 
+  {
     text: "truc",
-    id: id++
+    id: id++,
   },
-  { 
+  {
     text: "machin",
-    id: id++
+    id: id++,
   },
-  { 
+  {
     text: "bidule",
-    id: id++
-  }
-])
-const newItem = ref("")
+    id: id++,
+  },
+]);
+const newItem = ref("");
 
 function addItem() {
- if (newItem.value.length === 0) return
+  if (newItem.value.length === 0) return;
 
-//  items.value = [...items.value, { text: newItem.value, id: id++ } ]
- items.value.push({ text: newItem.value, id: id++ })
- newItem.value = '' 
+  //  items.value = [...items.value, { text: newItem.value, id: id++ } ]
+  items.value.push({ text: newItem.value, id: id++ });
+  newItem.value = "";
 }
 
-function removeItem(itemToDelete) {  
-  items.value = items.value.filter((item) => item !== itemToDelete)
+function removeItem(itemToDelete) {
+  items.value = items.value.filter((item) => item !== itemToDelete);
 }
 </script>
 
 <template>
   <main>
     <h1>{{ message }}</h1>
+    <HelloWorld />
     <ul>
       <li v-for="item in items" :key="item.id">
         {{ item.text }}
         <button @click="removeItem(item)">X</button>
       </li>
     </ul>
-    <input type="text" v-model="newItem">
-    <button v-show="newItem.length > 0" @click="addItem">Ajouter à la liste</button>
+    <input type="text" v-model="newItem" />
+    <button v-show="newItem.length > 0" @click="addItem">
+      Ajouter à la liste
+    </button>
   </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ bases-vuejs/vue1-bases/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+
+const message = ref("Hello from component");
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
